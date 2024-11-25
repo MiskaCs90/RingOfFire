65c5d7c36bf89e2df0a5390494443428f2dbb3d5
MiskaCs90 <162002700+MiskaCs90@users.noreply.github.com>
2024-11-25 21:11:34 +0100

    Start Menu Designed

---
+++ src/app/startscreen/startscreen.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startscreen',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './startscreen.component.html',
+  styleUrl: './startscreen.component.scss'
+})
+export class StartscreenComponent {
+
+}
